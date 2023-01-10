650ddf3de37077c868f8efd980812150cf3c27a0
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-01-09 23:24:39 -0600

    created findallRooms and createRoom controllers

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,12 +1,39 @@
-
+import Room from "../models/Room.js";
 
 const AllRooms = async (req, res) => {
-    res.status(200).json({
+    try {
+        const rooms = await Room.find();
+    
+      res.status(200).json({
         success: true,
-        message: 'All Rooms'
-    })
-}
+        count: rooms.length,
+        message: rooms,
+      });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
+};
 
-export {
-    AllRooms
-}
+// Create new room => /api/room
+const NewRoom = async (req, res) => {
+  try {
+    const room = await Room.create(req.body);
+    res.status(201).json({
+      success: true,
+      
+      room,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export { AllRooms, NewRoom };
--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,11 +1,12 @@
 import nc from 'next-connect';
 import dbConnect from '../../../config/dbConnect';
-import {AllRooms} from '../../../controllers/roomControllers';
+import {AllRooms, NewRoom} from '../../../controllers/roomControllers';
 
 const handler = nc();
 dbConnect();
 
 handler.get(AllRooms);
+handler.post(NewRoom);
 
 export default handler;
 
