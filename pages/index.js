49f7770e75b307e155190749deaff1496a57f6ac
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-01-11 11:06:58 -0600

    home page complete

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Home from "../components/Home"
+import Layout from "../components/layout/Layout"
 
-
-export default function Home() {
+export default function Index() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
   )
 }
