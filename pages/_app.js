7cfcfaf4e48ad341de6d9f05080004a1602d1924
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-01-11 14:52:01 -0600

    installed redux, thunk and redux toolkit, and created redux folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
+import { wrapper } from '../redux/store'
 
-export default function App({ Component, pageProps }) {
+ function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+
+export default wrapper.withRedux(App)
---
+++ redux/reducers/reducers.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+
+const reducers = combineReducers({
+    // Add reducers here
+    
+});
+
+export default reducers;
